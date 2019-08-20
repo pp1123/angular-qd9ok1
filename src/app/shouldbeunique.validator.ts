a1d75f7b505852d87dbf029c9c3a93dc136b90c8
pp1123 <pppiyush303@gmail.com>
2019-08-20 18:11:42 +0530

    2 files modified

--- src/app/shouldbeunique.validator.ts
+++ src/app/shouldbeunique.validator.ts
@@ -1,19 +1,19 @@
-// import { AbstractControl, Validators, ValidationErrors } from '@angular/forms';
-// import { Response, Http } from '@angular/http';
-// import { Injectable } from '@angular/core';
-// import { readFile } from 'fs';
+import { AbstractControl, Validators, ValidationErrors } from '@angular/forms';
+import { Response, Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 
+import * as resolve from 'fs';
 
-// export class ValidatorUnique {
-//   // static shouldbeunique(control: AbstractControl): Promise<ValidationErrors | null> {
-//   //   return new Promise((resolve, reject))=> {
-//   //     setTimeout(() => {
-//   //       if (control.value === "Piyush")
-//   //          resolve({ shouldbeunique: true });
-//   //       else
-//   //         reject(null);
-//   //     }, 2000);
-//   //   }
+export class ValidatorUnique {
+  static shouldbeunique(control: AbstractControl): Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject))=> {
+      setTimeout(() => {
+        if (control.value === "Piyush")
+           resolve({ shouldbeunique: true });
+        else
+          reject(null);
+      }, 2000);
+    }
  
-//   // }
-// }
+  }
+}
