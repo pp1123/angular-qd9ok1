17416174791696bbd8f1b29caba633910d2b31e9
pp1123 <pppiyush303@gmail.com>
2019-09-10 13:31:37 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule ,ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
  import {HttpModule} from '@angular/http';
@@ -9,10 +9,16 @@ import { SignupFormComponent } from './sign-up.component';
 import { newcoursearray } from './newcoursearray.component';
 import { ChangePasswordComponent } from './changepassword.component';
 import { posts } from './posts.component';
+import  {PostsService} from './posts.service';
+import   {AppErrorHandler} from './common/app-error-handler';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ReactiveFormsModule,HttpModule],
   declarations: [AppComponent, HelloComponent, ChildParentClass, SignupFormComponent, newcoursearray, ChangePasswordComponent, posts],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers:[
+    PostsService,
+  {provide:ErrorHandler,useClass:AppErrorHandler}
+   ]
 })
 export class AppModule { }
