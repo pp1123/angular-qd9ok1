c63835248815f642375257aa26c8761d3982563c
pp1123 <pppiyush303@gmail.com>
2019-09-16 15:11:52 +0530

    6 files modified, 4 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,43 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ChildParentClass } from './child.component';
 import { SignupFormComponent } from './sign-up.component';
-import { newcoursearray } from './newcoursearray.component';
+
 import { ChangePasswordComponent } from './changepassword.component';
 import { posts } from './posts.component';
 import  {PostsService} from './posts.service';
 import   {AppErrorHandler} from './common/app-error-handler';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { GithubFollowersComponent } from'./github-followers/github-followers.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from '@angular/material';
+const MATERIAL_MODULES = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+]
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule,HttpModule,
+  exports: [MATERIAL_MODULES],
+  imports: [MATERIAL_MODULES]
+})
+export class MaterialModule { }
+@NgModule({
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule,HttpModule,MaterialModule,
   RouterModule.forRoot([
    {
        path:"",component:HomeComponent
@@ -37,7 +61,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
      path:"**",component:NotFoundComponent
    }
   ])],
-  declarations: [AppComponent, HelloComponent, ChildParentClass, SignupFormComponent, newcoursearray, ChangePasswordComponent, posts, NavbarComponent, HomeComponent, GithubFollowersComponent, GithubProfileComponent, NotFoundComponent],
+  declarations: [AppComponent, HelloComponent, ChildParentClass, SignupFormComponent, ChangePasswordComponent, posts, NavbarComponent, HomeComponent, GithubFollowersComponent, GithubProfileComponent, NotFoundComponent],
   bootstrap: [AppComponent],
   providers:[
     PostsService,
